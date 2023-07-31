4e6f8ce1f53d90f8f3afd76ce3e488e87991b4cc
Dmitri Akatov <dmitri@akatov.com>
2023-07-31 15:23:30 +0200

    emacs wakatime-mode config with api key in 1password

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -2,4 +2,12 @@
 
 (org-babel-load-file
  (expand-file-name "readme.org"
-		   user-emacs-directory))
+                   user-emacs-directory))
+
+(add-to-list 'load-path
+             (expand-file-name "lisp"
+                               user-emacs-directory))
+
+(provide 'init)
+
+;;; init.el ends here
