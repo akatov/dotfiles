9afd1c0c431d721da336ca53eac9eeec2f2bb89e
Dmitri Akatov <dmitri@akatov.com>
2023-10-19 18:11:04 +0100

    deal with clojure customizations later

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -280,6 +280,19 @@
 (use-package clojure-mode
   :demand t)
 
+;; TODO - set this up properly
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(safe-local-variable-values
+;;    '((eval progn
+;; 	   (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+;; 	   (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))
+;;      (cider-repl-display-help-banner)
+;;      (cider-prefer-build-tool . clojure-cli))))
+
 (use-package company
   :demand t
   ;; :init
